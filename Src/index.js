8f44873e26505b2a4479b52172543c8e7ec09c0a
AleDias <70322758+AleDiass@users.noreply.github.com>
2020-11-24 17:27:46 -0300

    Add files via upload

---
+++ Src/index.js
@@ -0,0 +1,78 @@
+const readline = require('readline');
+const puppeteer = require('puppeteer');
+
+
+const inputUser = readline.createInterface({
+  input:process.stdin,
+  output:process.stdout
+})
+
+inputUser.question("Twitch User Target: " , (response)=>{
+  UserTarget = response;
+  inputUser.close();
+  if(UserTarget){
+    StartPuppeteer(UserTarget.toLowerCase())
+  }else{
+    console.log('Insert a Valid User')
+  }
+
+  })
+
+
+
+
+const StartPuppeteer = async(User) => {
+    
+  
+    console.log("Starting Services.... Await") 
+    const browser = await puppeteer.launch({headless:true});
+    const page = await browser.newPage();
+    await page.goto('http://twitch.com/' + User,{waitUntil:"networkidle2",timeout:0});
+    
+     
+    
+    const CheckUserStatus = await page.evaluate(()=>{
+      
+     try{
+       
+      const OfflineCheck = document.getElementsByClassName('channel-status-info channel-status-info--offline tw-border-radius-medium tw-inline-block')[0];
+      const OnlineCheck = document.getElementsByClassName('tw-strong tw-upcase tw-white-space-nowrap')[0]
+    
+      if(!OfflineCheck && !OnlineCheck){
+        return {error:"User not Found"}
+
+      }
+      else if(OfflineCheck){
+        
+        return {online:false , error:false}
+      }
+      else if(OnlineCheck){
+        const viewers = document.querySelector("p[data-a-target='animated-channel-viewers-count']").innerText
+        const StreamTime = document.getElementsByClassName('live-time')[0].innerText
+        
+        return {online:true,viewers,StreamTime, error:false}
+      }
+
+      
+     }catch(e){
+        console.log('Error 504 Try Again')
+     }
+      
+
+    })
+  
+    if(CheckUserStatus.error){
+      console.log(" \n \n " + CheckUserStatus.error)
+    }
+    if(!CheckUserStatus.online && !CheckUserStatus.error){
+      console.log(` \n \n ${User} is Offline`)
+    }
+    if(CheckUserStatus.online){
+      console.log(` \n \n User: ${User} \n Live Stream Started At:  ${CheckUserStatus.StreamTime} \n spectators: ${CheckUserStatus.viewers}`, )
+    }
+      
+    
+    
+    browser.close();
+
+}; 
